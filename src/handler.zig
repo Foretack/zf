b702d08062e6925cd135e604449c6826a67434b0
Foretack <69414142+Foretack@users.noreply.github.com>
2024-02-15 18:54:37 +0100

    f

--- src/handler.zig
+++ src/handler.zig
@@ -225,7 +225,7 @@ pub const Handler = struct {
             }
         }
 
-        dirSize -= freed;
+        dirSize -= @as(usize, freed);
         std.log.info("Freed {any} byes\n", .{freed});
     }
 
